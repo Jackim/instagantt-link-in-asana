cca21464954be400dab6fccf33dee479a437bacd
Jack Graham <jackbanna@gmail.com>
2018-09-13 11:30:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	var nav = document.getElementsByClassName("SidebarTopNavLinks Sidebar-section Sidebar-firstSection");
+	var nav = document.getElementsByClassName("SidebarTopNavLinks Sidebar-section Sidebar-firstSection")[0];
     var gantt = document.createElement('a');
     gantt.innerHTML = '<a class="NavigationLink SidebarItemRow--leftAlign SidebarItemRow SidebarTopNavLinks" href="https://instagantt.com"><span class="SidebarItemRow-name" title="My Tasks">Instagantt</span></a>';
     nav.appendChild(gantt);
